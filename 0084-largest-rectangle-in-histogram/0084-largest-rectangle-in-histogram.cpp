e3583a9ae0595063f0a16017accd709908d02e7e
Vedant Kesarwani <78657126+InventedSarawak@users.noreply.github.com>
2026-09-24 02:54:46 +0530

    Time: 23 ms (54.16%), Space: 81.2 MB (81.52%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
@@ -6,16 +6,15 @@ public:
         int maxArea = 0;
 
         for (int i = 0; i <= len; i++) {
-            int curr;
-            if (i == len) curr = 0;
-            else curr = heights[i];
+            int curr = 0;
+            if (i != len) curr = heights[i];
 
             while (!st.empty() && curr < heights[st.top()]) {
                 int height = heights[st.top()];
+                int width = i;
                 st.pop();
-                int width;
-                if (st.empty()) width = i;
-                else width = i - st.top() - 1;
+                
+                if (!st.empty()) width = i - st.top() - 1;
                 maxArea = max(maxArea, width * height);
             }
 
